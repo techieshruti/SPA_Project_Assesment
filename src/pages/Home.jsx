fcefb7e743268086933bf7a346b73f5d9c174d89
techieshruti <shrutichandra721@gmail.com>
2025-12-27 23:56:43 +0530

    added Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,13 @@ const Home = () => {
     <div>
         <div className="min-h-[calc(100vh-64px)] flex items-center justify-center px-6">
       <div className="max-w-xl text-center">
+        {/* heading */}
         <h1 className="text-3xl md:text-4xl font-bold text-slate-800 mb-4">
           Welcome to Product Explorer
         </h1>
+
         <p className="text-slate-600 mb-6">
-          Browse products fetched from a public API and view detailed information
-          using a clean and responsive single page application.
+          api.
         </p>
         <button className="bg-slate-900 text-white px-6 py-3 rounded-md hover:bg-slate-800 transition">
           Explore Products
