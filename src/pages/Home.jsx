9a0406accc485b36ba37bf4881d553e2aa70c8e6
techieshruti <shrutichandra721@gmail.com>
2025-12-27 23:49:17 +0530

    added Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <div className="min-h-[calc(100vh-64px)] flex items-center justify-center px-6">
+      <div className="max-w-xl text-center">
+        
+      </div>
+      </div>
+    </div>
   )
 }
 
