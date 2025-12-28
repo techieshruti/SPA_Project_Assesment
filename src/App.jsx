9b2b4e2fb6a96fd89c670ba4d3d3752033ddc576
techieshruti <shrutichandra721@gmail.com>
2025-12-28 17:10:38 +0530

    added products card styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
+import ProductCard from './components/ProductCard'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       {/* <Home/> */}
+      <ProductCard/>
     </div>
   )
 }
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div className="cursor-pointer border border-gray-100 rounded-lg p-4 shadow-sm hover:shadow-lg transition bg-gray-100 w-50 mx-6 my-4
+    ">
+        <div>
+            <div className="h-40 flex items-center justify-center mb-4">
+        <img
+          src="https://images-cdn.ubuy.co.in/660129b8cf52ac68f976755e-plus-size-women-s-lace-patchwork-tops.jpg"
+          alt=""
+          className="h-full object-contain rounded-xl"
+        />
+      </div>
+      <h3 className="text-sm font-semibold text-slate-800 line-clamp-2 mb-2">
+        Tops
+      </h3>
+      <p className="text-slate-900 font-bold">₹200.00</p>
+        </div>
+    </div>
+  )
+}
+
+export default ProductCard
