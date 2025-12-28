2101c317f08709772dd7f25dfa284b7a058e1612
techieshruti <shrutichandra721@gmail.com>
2025-12-28 19:52:25 +0530

    added ProductDetails page

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
 
 const ProductDetails = () => {
+    const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  
   return (
     <div>ProductDetails</div>
   )
