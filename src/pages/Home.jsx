b815011cebc452efe83d811ffea949de241085ec
techieshruti <shrutichandra721@gmail.com>
2025-12-28 19:39:32 +0530

    added styling to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,10 @@ const Home = () => {
     <div>
         <div className="min-h-[calc(100vh-64px)] flex items-center justify-end px-6 bg-[url('https://i.pinimg.com/736x/43/d1/35/43d135d38689527d117c56015d80a458.jpg')] bg-no-repeat bg-cover">
       <div className="max-w-md mr-20 p-6 text-center rounded-2xl
-        bg-white/70 backdrop-blur-md shadow-lg border border-white/40">
+        bg-white/30 backdrop-blur-md shadow-lg border border-white/40">
         {/* heading */}
         <h1 className="text-3xl md:text-4xl font-bold mb-4">
-           <h2>Welcome to</h2> ShopEase
+           Welcome to <span className="text-slate-800">ShopEase</span>
         </h1>
 
         <p className="mb-6">
